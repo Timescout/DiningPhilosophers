ef25eb6879fb59ec64aa0fc3278062e5bcf6887b
Timescout <timepolicescout@gmail.com>
2026-05-13 18:36:26 -0700

    added tests for get and set functions.

--- code/tests/PhilosopherTest.h
+++ code/tests/PhilosopherTest.h
@@ -13,3 +13,31 @@ TEST(PhilosopherSimulationUnitTest, DefaultConstructor_Works)
 {
     EXPECT_NO_THROW(Philosophers::PhilosopherSimulation testSim);
 }
+
+TEST(PhilosopherSimulationUnitTest, SetNumberPhilosophers_0_Throws) 
+{
+    Philosophers::PhilosopherSimulation testSim;
+
+    EXPECT_THROW(testSim.setNumberPhilosophers(0), std::out_of_range);
+}
+
+TEST(PhilosopherSimulationUnitTest, SetNumberPhilosophers_2_DoesNotThrow)
+{
+    Philosophers::PhilosopherSimulation testSim;
+
+    EXPECT_NO_THROW(testSim.setNumberPhilosophers(2));
+}
+
+TEST(PhilosopherSimulationUnitTest, SetSimulationTimeFrame_SameValue_DoesNotThrow)
+{
+    Philosophers::PhilosopherSimulation testSim;
+
+    EXPECT_NO_THROW(testSim.setSimulationTimeFrame(0, 0));
+}
+
+TEST(PhilosopherSimulationUnitTest, SetSimulationTimeFrame_SmallerTimeoutTime_Throws)
+{
+    Philosophers::PhilosopherSimulation testSim;
+
+    EXPECT_THROW(testSim.setSimulationTimeFrame(1, 0), std::logic_error);
+}
